c05d71776184449869496557a3329827edcc25f7
Ansh Bansal <anshbansal18106@gmail.com>
2023-09-13 01:06:41 +0530

    lets start

---
+++ server/src/main/java/com/example/SeatLocker/SeatLockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.SeatLocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatLockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatLockerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/SeatLocker/SeatLockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SeatLocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatLockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
